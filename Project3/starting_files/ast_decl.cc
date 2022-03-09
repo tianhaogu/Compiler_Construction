84eacb4599cc7495fb9d2a4ddcadf0e57bd44a6b
Tianchi Zhang <tonyztc@umich.edu>
2022-03-08 20:34:45 -0500

    vd fd

--- Project3/starting_files/ast_decl.cc
+++ Project3/starting_files/ast_decl.cc
@@ -12,6 +12,14 @@ Decl::Decl(Identifier *n) : Node(*n->GetLocation())
     (id = n)->SetParent(this);
 }
 
+// bool Decl::ConflictsWithPrevious(Decl *prev)
+// {
+//     if (prev == NULL || prev == this)
+//         return false;
+//     ReportError::DeclConflict(this, prev);
+//     return true;
+// }
+
 VarDecl::VarDecl(Identifier *n, Type *t) : Decl(n)
 {
     Assert(n != NULL && t != NULL);
